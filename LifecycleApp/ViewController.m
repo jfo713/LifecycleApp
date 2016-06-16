f4de2b728219de16cd3eb2cc014267f73e529de2
James <Hal@Jamess-MacBook-Pro.local>
2016-06-16 13:55:44 -0500

    LifecycleAppCorrected

--- LifecycleApp/ViewController.m
+++ LifecycleApp/ViewController.m
@@ -17,11 +17,14 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"viewDidLoad"); // When -(void)viewDidLoad fires, display 'viewDidLoad' in console.
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+    NSLog(@"didReceiveMemoryWarning");
 }
 
 @end
